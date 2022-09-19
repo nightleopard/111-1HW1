d2295880336b6c72b6a61930840372f215ddaf13
nightleopard <win20011018@gmail.com>
2022-09-19 12:51:03 +0800

    HW1

---
+++ 111-1HW1/Test.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1HW1
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Hello App");
+        }
+
+        protected System.Void Button1_Click(System.Object sender, System.EventArgs e)
+        {
+            Response.Write("Hello Button");
+        }
+    }
+}
