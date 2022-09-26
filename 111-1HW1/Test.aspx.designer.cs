bb6ffc62191b19adbbba5edfa7aa788de7846e40
nightleopard <win20011018@gmail.com>
2022-09-26 10:27:39 +0800

    HW1

--- 111-1HW1/Test.aspx.designer.cs
+++ 111-1HW1/Test.aspx.designer.cs
@@ -24,12 +24,12 @@ namespace _111_1HW1
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Button1 控制項。
+        /// btn_Show 控制項。
         /// </summary>
         /// <remarks>
         /// 自動產生的欄位。
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button btn_Show;
     }
 }
